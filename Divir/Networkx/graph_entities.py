b3ecbb3eb3cba3cc4f4667ea965f209e865e6b6a
divir94 <divir94@gmail.com>
2014-08-10 14:41:09 -0400

    heat diffusion data

--- Divir/Networkx/graph_entities.py
+++ Divir/Networkx/graph_entities.py
@@ -1,12 +1,16 @@
 # -*- coding: utf8 -*-
 from pprint import pprint
 from random import choice
+from scipy.sparse.csgraph import laplacian
+from scipy.linalg import expm
 from datetime import date
 import time
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 import shelve
 import itertools
+import sys
 
 """ ----------------- Helper  ------------------"""
 
@@ -40,20 +44,32 @@ class EntitiyGraph():
         self.date = date
         self.date_str = str(date)
         self.path_to_entities = "../Data/Entities/"
-        self.path_to_graph = "../Data/Graphs/"
+        self.path_to_graph = "../Data/Heat-Graphs/"
+
+    def filter_node_type(self, type):
+        noise = ['Anniversary', 'Date', 'EmailAddress', 'FaxNumber', 'PhoneNumber', 'URL', 'PersonEmailAddress']
+        return type in noise
+
+    def filter_node_size(self, G, threshold):
+        for name in G.nodes():
+            if G.node[name]['size'] < threshold:
+                G.remove_node(name)
+        return G
 
     def add_entities_to_graph(self, G, entities):
         entity_names = []
         # add nodes
         for entity in entities:
-            try:
+            if isinstance(entity, dict) and 'name' in entity:
+                if self.filter_node_type(entity['_type']): continue
                 name = entity['name']
                 entity_names.append(name)
                 if name in G:
                     G.node[name]['size'] += 1
                 else:
                     G.add_node(name, type=entity['_type'], size=1)
-            except: print "Failed to print name"
+            else:
+                print "Failed to get entity name. %s" % type(entity)
 
         num_entities = len(entity_names)
 
@@ -67,6 +83,43 @@ class EntitiyGraph():
                     G.add_edge(source, target, weight=1 )
         return G
 
+    def setEdgeWeights(self, G, adj_matrix, threshold):
+        '''
+        :param G: networkx undirected graph
+        :param adj_matrix: numpy adj matrix with order G.nodes()
+        :return: G with edge weights from adj_matrix
+        '''
+        size = len(G)
+        nodes = G.nodes()
+        for i in range(1, size):
+            for j in range(i):
+                source, target = nodes[i], nodes[j]
+                if G.has_edge(source, target):
+                    weight = "%.3f" % adj_matrix.item((i, j))
+                    if weight < threshold:
+                        G.remove_edge(source, target)
+                    else:
+                        G.add_edge(source, target, weight=weight)
+        return G
+
+    def getHeatMatrix(self, G):
+        print "adj matrix"
+        start_time = time.time()
+        A = nx.to_numpy_matrix(G)
+        print time.time() - start_time
+
+        print "laplacian"
+        start_time = time.time()
+        L = laplacian(A)
+        print time.time() - start_time
+
+        print "heat expm"
+        start_time = time.time()
+        heat_matrix = expm(-0.6*L)
+        print time.time() - start_time
+
+        return heat_matrix
+
     def run_store(self):
         G = nx.Graph()
         start_time = time.time()
@@ -79,13 +132,24 @@ class EntitiyGraph():
             counter +=1
 
         entities_db.close()
+
+        # filter nodes by size
+        G = self.filter_node_size(G, 5)
+
+        # heat diffusion matrix
+        heat_matrix = self.getHeatMatrix(G)
+
+        # make graph
+        median = np.median(heat_matrix)
+        H = self.setEdgeWeights(G, heat_matrix, 0)
+
         nx.write_gexf(G, self.path_to_graph + self.date_str + '.gexf')
         print "Time taken: %s sec" % str(time.time() - start_time)
 
 
 
 """ ------------------- Main ------------------"""
-for i in range(3,10):
+for i in range(1,2):
     my_date = date(2014, 7, i)
     extractor = EntitiyGraph(my_date)
     extractor.run_store()
--- Divir/Networkx/heat_diffusion.py
+++ Divir/Networkx/heat_diffusion.py
@@ -1,8 +1,11 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+from scipy.sparse.csgraph import laplacian
+from scipy.linalg import expm3
 from pprint import pprint
-from scipy import linalg
+
 
 def createGraph():
     G = nx.Graph()
@@ -30,37 +33,50 @@ def setEdgeWeights(G, adj_matrix, threshold):
     for i in range(1, size):
         for j in range(i):
             source, target = nodes[i], nodes[j]
-            weight = "%.3f" % adj_matrix.item((i, j))
-            if weight < threshold:
-                G.remove_edge(source, target)
-            else:
-                G[source][target]['weight'] = weight
+            if G.has_edge(source, target):
+                weight = "%.3f" % adj_matrix.item((i, j))
+                if weight < threshold:
+                    G.remove_edge(source, target)
+                else:
+                    G.add_edge(source, target, weight=weight)
     return G
 
 def getHeatMatrix(G):
-    L = nx.laplacian_matrix(G)
-    L = L.todense()
-    heat_matrix = linalg.expm(-L)
+    A = nx.to_numpy_matrix(G)
+    L = laplacian(A)
+    start_time = time.time()
+    heat_matrix = expm3(-L)
+    print time.time() - start_time
     return heat_matrix
 
 def drawGraph(G):
     pos = nx.spring_layout(G)
     # nodes
-    nx.draw_networkx_nodes(G,pos,node_size=600, node_color="white")
+    nx.draw_networkx_nodes(G,pos, node_size=600, node_color="white")
     # edges
-    nx.draw_networkx_edges(G,pos, width=6,alpha=0.5,edge_color='black')
+    nx.draw_networkx_edges(G,pos, width=6, alpha=0.5, edge_color='black')
     # labels
-    nx.draw_networkx_labels(G,pos,font_size=20,font_family='sans-serif')
-    nx.draw_networkx_edge_labels(G, pos,  label_pos=0.3)
+    nx.draw_networkx_labels(G,pos, font_size=20, font_family='sans-serif', label_pos=0.3)
+    nx.draw_networkx_edge_labels(G, pos)
     # axis and save
     plt.axis('off')
-    plt.savefig('heat-diffusion-2.png')
+    plt.savefig('heat-diffusion-3.png')
 
+def testGraph(edgeList):
+    G = nx.Graph()
+    G.add_edges_from(edgeList, weight=1)
+    return G
 
-G = createGraph()
+# make graph
+G = testGraph([('A','B'),('A','C'), ('C', 'B'), ('E','C'), ('C', 'D')])
+# heat diffusion matrix
 heat_matrix = getHeatMatrix(G)
+np.savetxt('heat matrix', heat_matrix)
+# make graph
 H = setEdgeWeights(G, heat_matrix, 0)
-print H.edge['A']['B']
+# draw graph
+drawGraph(G)
+#nx.write_gexf(H, 'heat.gexf')
 
 
 
