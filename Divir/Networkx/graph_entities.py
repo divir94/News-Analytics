7a69283b762d7519557dc16d0a91918d81c8c18e
divir94 <divir94@gmail.com>
2014-08-12 23:27:19 -0400

    added price changes

--- Divir/Networkx/graph_entities.py
+++ Divir/Networkx/graph_entities.py
@@ -11,30 +11,9 @@ import numpy as np
 import shelve
 import itertools
 import sys
-
-""" ----------------- Helper  ------------------"""
-
-def merge_graph(G, H):
-    '''
-    :param G: Graph to add nodes and edges to
-    :param H: Graph to add nodes and edges from
-    :return: merged graph G
-    '''
-    nodeList = H.nodes(data=True)
-    edgeList = H.edges(data=True)
-
-    # add nodes from H to G
-    for node in nodeList:
-        if not G.has_node(node): G.add_nodes_from([node])
-
-    # add edges from H to G
-    for edge in edgeList:
-        source, target, weight =  edge
-        if G.has_edge(source, target):
-            G[source][target]['weight'] += H[source][target]['weight']
-        else: G.add_edges_from([edge])
-
-    return G
+import copy
+import Quandl
+import pandas as pd
 
 
 """ ----------------- EntitiyGraph Class  ------------------"""
@@ -44,7 +23,8 @@ class EntitiyGraph():
         self.date = date
         self.date_str = str(date)
         self.path_to_entities = "../Data/Entities/"
-        self.path_to_graph = "../Data/Heat-Graphs/"
+        self.path_to_graph = "../Data/Price-Heat-Graphs/"
+        self.num_stock_prices = 0
 
     def filter_node_type(self, type):
         noise = ['Anniversary', 'Date', 'EmailAddress', 'FaxNumber', 'PhoneNumber', 'URL', 'PersonEmailAddress']
@@ -56,20 +36,75 @@ class EntitiyGraph():
                 G.remove_node(name)
         return G
 
+    def get_stock_price(self, entity, ticker_db):
+        # check if ticker in entity
+        if 'resolutions' in entity and 'ticker' in entity['resolutions'][0]:
+            ticker = str(entity['resolutions'][0]['ticker'])
+
+            if ticker in ticker_db:
+                data = ticker_db[ticker]
+                if data.empty:
+                    print "Found in db - Ticker: %s, Empty" % (ticker)
+                    return [ticker]+[0]*3
+                open = data['Open'][self.date_str]
+                close = data['Close'][self.date_str]
+                price_change = float('%.2f' % ( (close-open)*100 / float(open) ))
+
+                self.num_stock_prices += 1
+                print "Found in db - Ticker: %s, Price change: %.2f" % (ticker, price_change)
+                return ticker, open, close, price_change
+
+
+            # get price change
+            try:
+                code = ''
+
+                # find code for a dataset with 1) stock prices 2) daily frequency
+                datasets = datasets = Quandl.search(ticker +' stock price', verbose = False)
+                for dataset in datasets:
+                    if  dataset['freq']=='daily' and 'Open' in dataset['colname']:
+                        code = dataset['code']
+                        break
+                print ticker, code
+                if code == '': code = 'GOOG/NASDAQ_'+ticker
+
+                # get stock prices
+                data = Quandl.get(code, trim_start=self.date_str, trim_end=self.date_str, collapse="daily", authtoken="EqhJyL2Ywt3Q-hxydsN3")
+                 # log
+                self.num_stock_prices += 1
+                ticker_db[ticker] = data
+
+                open = data['Open'][self.date_str]
+                close = data['Close'][self.date_str]
+                price_change = float('%.2f' % ( (close-open)*100 / float(open) ))
+                print "Ticker: %s, Price change: %.2f" % (ticker, price_change)
+                return ticker, open, close, price_change
+
+            except:
+                ticker_db[ticker] = pd.DataFrame()
+                print "Exception: Ticker: %s, Price change not found" % (ticker)
+                return [ticker]+[0]*3
+        else:
+            return ['']+[0]*3
+
+
     def add_entities_to_graph(self, G, entities):
         entity_names = []
         # add nodes
         for entity in entities:
             if isinstance(entity, dict) and 'name' in entity:
-                if self.filter_node_type(entity['_type']): continue
                 name = entity['name']
+                type = entity['_type']
+                # filter node types
+                if self.filter_node_type(type): continue
                 entity_names.append(name)
+                # add/update node
                 if name in G:
                     G.node[name]['size'] += 1
                 else:
-                    G.add_node(name, type=entity['_type'], size=1)
+                    G.add_node(name, type=type, size=1, entity=entity)
             else:
-                print "Failed to get entity name. %s" % type(entity)
+                print "Failed to get entity name"
 
         num_entities = len(entity_names)
 
@@ -134,13 +169,34 @@ class EntitiyGraph():
         entities_db.close()
 
         # filter nodes by size
-        G = self.filter_node_size(G, 5)
+        print "Graph size before filtering by node size: %d" % len(G)
+        G = self.filter_node_size(G, 1)
+        print "Graph size after filtering by node size: %d" % len(G)
+
+        ticker_db = shelve.open(self.path_to_graph + 'ticker')
+        i = 0
+        for name in G:
+            if G.node[name]['type'] == 'Company':
+                entity = G.node[name]['entity']
+                ticker, open, close, price_change = self.get_stock_price(entity, ticker_db)
+
+                G.node[name]['ticker'] = ticker
+                G.node[name]['open'] = float(open)
+                G.node[name]['close'] = float(close)
+                G.node[name]['price_change'] = float(price_change)
+
+                if i%20 == 0:
+                    ticker_db.close()
+                    ticker_db = shelve.open(self.path_to_graph + 'ticker')
+                    print "%d: Saved tickers to db" % i
+                i += 1
+        ticker_db.close()
 
         # heat diffusion matrix
         heat_matrix = self.getHeatMatrix(G)
 
         # make graph
-        median = np.median(heat_matrix)
+        #median = np.median(heat_matrix)
         H = self.setEdgeWeights(G, heat_matrix, 0)
 
         nx.write_gexf(G, self.path_to_graph + self.date_str + '.gexf')
@@ -149,7 +205,7 @@ class EntitiyGraph():
 
 
 """ ------------------- Main ------------------"""
-for i in [2,3,4,6,7,8,9,10]:
+for i in [2]:
     my_date = date(2014, 7, i)
     extractor = EntitiyGraph(my_date)
     extractor.run_store()
--- Divir/Networkx/test.py
+++ Divir/Networkx/test.py
@@ -1,22 +1,60 @@
-from matplotlib import pyplot as plt
-import networkx as nx
-G = nx.Graph()
-G.add_edge(1,2)
-G.add_edge(2,3)
-for v in G.nodes():
-    G.node[v]['state']='X'
-G.node[1]['state']='Y'
-G.node[2]['state']='Y'
+import datetime as dt
+import sys
+import Quandl
 
-for n in G.edges_iter():
-    G.edge[n[0]][n[1]]['state']='X'
-G.edge[2][3]['state']='Y'
+def getValueChange(self, ticker, start, end):
+    import Quandl
+    from dateutil.relativedelta import relativedelta
+    stockPriceResponse = self.pastPrices.get(ticker, "fetch")
+    if stockPriceResponse=="fetch":
+        try:
+            #mrktCapResponse = Quandl.get("DMDRN/"+ticker+"_MKT_CAP", trim_start=str(start), trim_end=str(end), collapse="annual", authtoken="xaYsva9sQfpTwzq7NVoz")
+            stockPriceResponse =  Quandl.get("GOOG/NASDAQ_"+ticker, trim_start=str(start), trim_end=str(end), collapse="daily", authtoken="xaYsva9sQfpTwzq7NVoz")
 
-pos = nx.spring_layout(G)
+        except:
+            print "failed on "+ticker+" from ",start, " to ", end
+            return 0
+    #assert len(mrktCapResponse.index)==1
+    #mrktCap = mrktCapResponse[mrktCapResponse.keys()[0]][mrktCapResponse.index[0]]
+    while str(start) not in stockPriceResponse.Close:
+        start = start + relativedelta(days=1)
+        print "new start date ", start
+    while str(end) not in stockPriceResponse.Close:
+        end = end - relativedelta(days=1)
+        print "new end date ", end
+    startPrice = stockPriceResponse["Close"][str(start)]
+    endPrice = stockPriceResponse["Close"][str(end)]
+    return abs((endPrice-startPrice)/startPrice)>0.01
+#Example Usage:
+# print getPriceChange(AAPL, dt.date(2013, 1, 4), dt.date(2013, 2, 4))
 
-nx.draw(G, pos)
-node_labels = nx.get_node_attributes(G,'state')
-nx.draw_networkx_labels(G, pos, labels = node_labels)
-edge_labels = nx.get_edge_attributes(G,'state')
-nx.draw_networkx_edge_labels(G, pos, labels = edge_labels)
-plt.savefig('heat-diffusion.png')
+
+date = '2014-08-08'
+def get_stock_price(ticker):
+    # get price change
+    try:
+        code = ''
+
+        # find code for a dataset with 1) stock prices 2) daily frequency
+        datasets = datasets = Quandl.search(ticker +' stock price', verbose = False)
+        for dataset in datasets:
+            if  dataset['freq']=='daily' and 'Open' in dataset['colname']:
+                code = dataset['code']
+                break
+        print ticker, code
+        if code == '': code = 'GOOG/NASDAQ_'+ticker
+
+        # get stock prices
+        data = Quandl.get(code, collapse="daily", authtoken="EqhJyL2Ywt3Q-hxydsN3")
+        print data['Open']
+        print data['Open']['2001-07-05']
+        # open = data['Open'][date]
+        # close = data['Close'][date]
+        # price_change = float('%.2f' % ( (close-open)*100 / float(open) ))
+        # print "Ticker: %s, Price change: %f" % (ticker, price_change)
+        # return ticker, open, close, price_change
+    except:
+        print "Exception: Ticker: %s, Price change not found" % (ticker)
+        return [ticker]+[0]*3
+
+#get_stock_price('AAPL')
